f0776a9fe2eb90393696871fcde4c60336fc39ba
Nicholas Serra <nserra310@yahoo.com>
2020-01-08 18:37:46 -0800

    adding files to repo in order to deploy application to Heroku

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("South Western Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheese Burger", true);
